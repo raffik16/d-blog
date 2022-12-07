83eddcfe9d2605d62a8d8fa6dc9c5227348a5f01
Raffi Keklikian <69125695+raffik16@users.noreply.github.com>
2022-12-07 09:05:10 -0800

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -5,21 +5,7 @@ export default function Intro() {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Awesome posts found here{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and{' '}
-        <a
-          href="https://www.datocms.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          DatoCMS
-        </a>
-        .
+        Awesome posts found here{' '}.
       </h4>
     </section>
   )
