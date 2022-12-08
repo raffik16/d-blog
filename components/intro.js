025ab23088eb7af44360e6391597dc29d55965d9
Raffi Keklikian <69125695+raffik16@users.noreply.github.com>
2022-12-08 11:54:05 -0800

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -4,9 +4,9 @@ export default function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         🌞
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Somewhere In The Middle.
-      </h4>
+      </h2>
     </section>
   )
 }
